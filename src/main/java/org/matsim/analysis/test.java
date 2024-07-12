324f5946eb5fbc5a2c82196bf2e738a18c8d2636
OliBus801 <olivier2202@gmail.com>
2024-07-12 11:06:31 -0400

    Pushed RandomSeedAnalysis Baseline

--- src/main/java/org/matsim/analysis/test.java
+++ src/main/java/org/matsim/analysis/test.java
@@ -1,70 +1,42 @@
 package org.matsim.analysis;
 import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.GlobalConfigGroup;
 import org.matsim.core.controler.Controler;
-import org.matsim.core.controler.OutputDirectoryHierarchy;
-import org.matsim.simulation_directive.FactorialReplanningSimulationDirective;
-import org.matsim.simulation_directive.ReplanningSimulationDirective;
-import org.matsim.simulation_directive.ScoringSimulationDirective;
-import org.matsim.simulation_directive.SimulationDirective;
-
 
 import java.util.*;
 
 public class test {
 
-    public static final String baseOutputDirectory = "scenarios/equil/outputs//";
-    static String baselineConfig = "scenarios/equil/config.xml";
+    public static final String baseOutputDirectory = "scenarios/siouxfalls-2014/outputs/RandomSeedAnalysis/";
+    public static final String fixedRandomSeedSingleThreadDirectory = baseOutputDirectory + "Baseline/";
+    static String baselineConfig = "scenarios/siouxfalls-2014/configs/config_baseline_randomseed.xml";
 
-    static ScoringSimulationDirective moneyScoringSimulationDirective =
-            new ScoringSimulationDirective(
-                    "MarginalUtilityOfMoney",
-                    new double[]{1.0, 1.1});
-
-    static ReplanningSimulationDirective testReplanningSimulationDirective =
-            new ReplanningSimulationDirective(
-                    "ReRoute",
-                    new double[]{0.1, 0.2, 1.0});
-
-    static FactorialReplanningSimulationDirective factorialReplanningSimulationDirective =
-            new FactorialReplanningSimulationDirective(
-                    "ReRoute",
-                    "TimeAllocationMutator",
-                    new double[]{0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8});
+    public static List<List<Object>> experiments = new ArrayList<>();
 
     public static void main(String[] args){
-        System.out.println("Hello World ! - From a Docker Container !");
-    }
 
-    public static void runSimulations(SimulationDirective directive, String baselineConfig, String baseOutputDirectory) {
-        for (double value : directive.getParameterValues()) {
-            // Create the specific config from the directive and the baselineConfig
-            Config modifiedConfig = directive.modifyConfig(baselineConfig, value);
-
-            // Set up the output directory and enable overriding
-            String outputFolder = directive.getParameterName() + "_" + value;
-            String outputDirectory = baseOutputDirectory + outputFolder;
-            modifiedConfig.controller().setOutputDirectory(outputDirectory);
-            modifiedConfig.controller().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
-
-            // Create an instance of the controller using current config and run simulation
-            Controler controler = new Controler(modifiedConfig);
-            controler.run();
+        // Adding Experiments Parameters into a list
+        for (int i = 1; i < 11; i++) {
+            experiments.add(Arrays.asList((long) 24, 1, fixedRandomSeedSingleThreadDirectory + i));
         }
-    }
 
-    public static void runFactorialSimulations(FactorialReplanningSimulationDirective directive, String baselineConfig, String baseOutputDirectory) {
-        for (double[] value : directive.getPairedParameterValues()) {
-            // Create the specific config from the directive and the baselineConfig
-            Config modifiedConfig = directive.modifyConfig(baselineConfig, value);
+        // Iterate through the list
+        for(List<Object> experiment: experiments){
+            System.out.println("Running Experiment: " + experiment);
 
-            // Set up the output directory and enable overriding
-            String outputFolder = directive.getFactor1() + "_" + Math.round(value[0]*100) + directive.getFactor2() + "_" + Math.round(value[1]*100);
-            String outputDirectory = baseOutputDirectory + outputFolder;
-            modifiedConfig.controller().setOutputDirectory(outputDirectory);
-            modifiedConfig.controller().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+            // Load the baseline config and add a globalConfigGroup
+            Config config = ConfigUtils.loadConfig(baselineConfig);
+            GlobalConfigGroup globalConfigGroup = ConfigUtils.addOrGetModule(config, GlobalConfigGroup.GROUP_NAME,
+                    GlobalConfigGroup.class);
 
-            // Create an instance of the controller using current config and run simulation
-            Controler controler = new Controler(modifiedConfig);
+            // Modify the config
+            globalConfigGroup.setRandomSeed((Long) experiment.get(0));
+            globalConfigGroup.setNumberOfThreads((Integer) experiment.get(1));
+            config.controller().setOutputDirectory((String) experiment.get(2));
+
+            // Run the simulation
+            Controler controler = new Controler(config);
             controler.run();
         }
     }
