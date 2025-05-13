002026dbec7b3a39b0cd3d6202db3a134aff8331
OliBus801 <olivier2202@gmail.com>
2025-05-13 10:53:15 -0400

    Add Kyoto Run Java Class Files

---
+++ src/main/java/org/matsim/project/OpenKyotoScenario.java
@@ -0,0 +1,127 @@
+package org.matsim.project;
+
+import com.google.inject.Key;
+import com.google.inject.name.Names;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.application.MATSimApplication;
+import org.matsim.application.options.SampleOptions;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.ReplanningConfigGroup;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule;
+import org.matsim.core.router.costcalculators.TravelDisutilityFactory;
+import org.matsim.core.router.util.TravelTime;
+import org.matsim.simwrapper.SimWrapperConfigGroup;
+import org.matsim.simwrapper.SimWrapperModule;
+import picocli.CommandLine;
+
+import java.util.List;
+
+@CommandLine.Command(header = ":: Open Kyoto Scenario ::", version = OpenKyotoScenario.VERSION, mixinStandardHelpOptions = true)
+public class OpenKyotoScenario extends MATSimApplication {
+
+	public static final String VERSION = "1.0";
+	public static final String CRS = "EPSG:32653";
+
+	private static final Logger log = LogManager.getLogger(OpenKyotoScenario.class);
+
+	@CommandLine.Mixin
+	private final SampleOptions sample = new SampleOptions(10, 25, 3, 1);
+
+	public OpenKyotoScenario() {
+		super(String.format("scenarios/kyoto/kyoto-v%s-10pct.config.xml", VERSION, VERSION));
+	}
+
+	public static void main(String[] args) {
+		MATSimApplication.run(OpenKyotoScenario.class, args);
+	}
+
+	@Override
+	protected Config prepareConfig(Config config) {
+
+		SimWrapperConfigGroup sw = ConfigUtils.addOrGetModule(config, SimWrapperConfigGroup.class);
+
+		if (sample.isSet()) {
+			double sampleSize = sample.getSample();
+
+			config.qsim().setFlowCapFactor(sampleSize);
+			config.qsim().setStorageCapFactor(sampleSize);
+
+			// Counts can be scaled with sample size
+			config.counts().setCountsScaleFactor(sampleSize);
+			sw.sampleSize = sampleSize;
+
+			config.controller().setRunId(sample.adjustName(config.controller().getRunId()));
+			config.controller().setOutputDirectory(sample.adjustName(config.controller().getOutputDirectory()));
+			config.plans().setInputFile(sample.adjustName(config.plans().getInputFile()));
+		}
+
+		Activities.addScoringParams(config, true);
+
+		// Required for all calibration strategies
+		for (String subpopulation : List.of("person")) {
+			config.replanning().addStrategySettings(
+				new ReplanningConfigGroup.StrategySettings()
+					.setStrategyName(DefaultPlanStrategiesModule.DefaultSelector.ChangeExpBeta)
+					.setWeight(1.0)
+					.setSubpopulation(subpopulation)
+			);
+
+			config.replanning().addStrategySettings(
+				new ReplanningConfigGroup.StrategySettings()
+					.setStrategyName(DefaultPlanStrategiesModule.DefaultStrategy.ReRoute)
+					.setWeight(0.15)
+					.setSubpopulation(subpopulation)
+			);
+
+			config.replanning().addStrategySettings(
+				new ReplanningConfigGroup.StrategySettings()
+				.setStrategyName(DefaultPlanStrategiesModule.DefaultStrategy.TimeAllocationMutator_ReRoute)
+				.setWeight(0.15)
+				.setSubpopulation(subpopulation)
+			);
+		}
+
+// TODO: modechoice is not calibrated yet
+//		config.replanning().addStrategySettings(
+//			new ReplanningConfigGroup.StrategySettings()
+//				.setStrategyName(DefaultPlanStrategiesModule.DefaultStrategy.SubtourModeChoice)
+//				.setWeight(0.15)
+//				.setSubpopulation("person")
+//		);
+
+		return config;
+	}
+
+	@Override
+	protected void prepareScenario(Scenario scenario) {
+	}
+
+	@Override
+	protected void prepareControler(Controler controler) {
+
+		controler.addOverridingModule(new SimWrapperModule());
+
+		controler.addOverridingModule(new TravelTimeBinding());
+	}
+
+	/**
+	 * Add travel time bindings for ride and freight modes, which are not actually network modes.
+	 */
+	public static final class TravelTimeBinding extends AbstractModule {
+		@Override
+		public void install() {
+			addTravelTimeBinding(TransportMode.ride).to(networkTravelTime());
+			addTravelDisutilityFactoryBinding(TransportMode.ride).to(carTravelDisutilityFactoryKey());
+
+			addTravelTimeBinding("freight").to(Key.get(TravelTime.class, Names.named(TransportMode.truck)));
+			addTravelDisutilityFactoryBinding("freight").to(Key.get(TravelDisutilityFactory.class, Names.named(TransportMode.truck)));
+		}
+	}
+
+}
