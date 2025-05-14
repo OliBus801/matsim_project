e57d2536a5b0cf24e01d4d894951edf0a3c8e03b
OliBus801 <olivier2202@gmail.com>
2025-05-14 17:41:53 -0400

    Add Berlin scenario files and update .gitignore for Berlin outputs

---
+++ src/main/java/org/matsim/project/OpenBerlinScenario.java
@@ -0,0 +1,161 @@
+package org.matsim.project;
+
+import com.google.inject.Key;
+import com.google.inject.name.Names;
+import org.matsim.analysis.personMoney.PersonMoneyEventsAnalysisModule;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.application.MATSimApplication;
+import org.matsim.application.options.SampleOptions;
+import org.matsim.contrib.bicycle.BicycleConfigGroup;
+import org.matsim.contrib.bicycle.BicycleLinkSpeedCalculator;
+import org.matsim.contrib.bicycle.BicycleLinkSpeedCalculatorDefaultImpl;
+import org.matsim.contrib.bicycle.BicycleTravelTime;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.ReplanningConfigGroup;
+import org.matsim.core.config.groups.VspExperimentalConfigGroup;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule;
+import org.matsim.core.router.costcalculators.OnlyTimeDependentTravelDisutilityFactory;
+import org.matsim.core.router.costcalculators.TravelDisutilityFactory;
+import org.matsim.core.router.util.TravelTime;
+import org.matsim.simwrapper.SimWrapperConfigGroup;
+import org.matsim.simwrapper.SimWrapperModule;
+import picocli.CommandLine;
+
+import java.util.List;
+
+@CommandLine.Command(header = ":: Open Berlin Scenario ::", version = OpenBerlinScenario.VERSION, mixinStandardHelpOptions = true, showDefaultValues = true)
+public class OpenBerlinScenario extends MATSimApplication {
+
+	public static final String VERSION = "6.4";
+	public static final String CRS = "EPSG:25832";
+
+	@CommandLine.Mixin
+	private final SampleOptions sample = new SampleOptions(10, 25, 3, 1);
+
+	@CommandLine.Option(names = "--plan-selector",
+		description = "Plan selector to use.",
+		defaultValue = DefaultPlanStrategiesModule.DefaultSelector.ChangeExpBeta)
+	private String planSelector;
+
+	public OpenBerlinScenario() {
+		super(String.format("input/v%s/berlin-v%s.config.xml", VERSION, VERSION));
+	}
+
+	public static void main(String[] args) {
+		MATSimApplication.run(OpenBerlinScenario.class, args);
+	}
+
+	@Override
+	protected Config prepareConfig(Config config) {
+
+		SimWrapperConfigGroup sw = ConfigUtils.addOrGetModule(config, SimWrapperConfigGroup.class);
+
+		if (sample.isSet()) {
+			double sampleSize = sample.getSample();
+
+			config.qsim().setFlowCapFactor(sampleSize);
+			config.qsim().setStorageCapFactor(sampleSize);
+
+			// Counts can be scaled with sample size
+			config.counts().setCountsScaleFactor(sampleSize);
+			sw.sampleSize = sampleSize;
+
+			config.controller().setRunId(sample.adjustName(config.controller().getRunId()));
+			config.controller().setOutputDirectory(sample.adjustName(config.controller().getOutputDirectory()));
+			config.plans().setInputFile(sample.adjustName(config.plans().getInputFile()));
+		}
+
+		config.qsim().setUsingTravelTimeCheckInTeleportation(true);
+
+		ActivitiesBerlin.addScoringParams(config, true);
+
+		// Required for all calibration strategies
+		for (String subpopulation : List.of("person", "freight", "goodsTraffic", "commercialPersonTraffic", "commercialPersonTraffic_service")) {
+			config.replanning().addStrategySettings(
+				new ReplanningConfigGroup.StrategySettings()
+					.setStrategyName(planSelector)
+					.setWeight(1.0)
+					.setSubpopulation(subpopulation)
+			);
+
+			config.replanning().addStrategySettings(
+				new ReplanningConfigGroup.StrategySettings()
+					.setStrategyName(DefaultPlanStrategiesModule.DefaultStrategy.ReRoute)
+					.setWeight(0.15)
+					.setSubpopulation(subpopulation)
+			);
+		}
+
+		config.replanning().addStrategySettings(
+			new ReplanningConfigGroup.StrategySettings()
+				.setStrategyName(DefaultPlanStrategiesModule.DefaultStrategy.TimeAllocationMutator)
+				.setWeight(0.15)
+				.setSubpopulation("person")
+		);
+
+		config.replanning().addStrategySettings(
+			new ReplanningConfigGroup.StrategySettings()
+				.setStrategyName(DefaultPlanStrategiesModule.DefaultStrategy.SubtourModeChoice)
+				.setWeight(0.15)
+				.setSubpopulation("person")
+		);
+
+		// Need to switch to warning for best score
+		if (planSelector.equals(DefaultPlanStrategiesModule.DefaultSelector.BestScore)) {
+			config.vspExperimental().setVspDefaultsCheckingLevel(VspExperimentalConfigGroup.VspDefaultsCheckingLevel.warn);
+		}
+
+		// Bicycle config must be present
+		ConfigUtils.addOrGetModule(config, BicycleConfigGroup.class);
+
+		return config;
+	}
+
+	@Override
+	protected void prepareControler(Controler controler) {
+
+		controler.addOverridingModule(new SimWrapperModule());
+
+		controler.addOverridingModule(new TravelTimeBinding());
+
+		controler.addOverridingModule(new PersonMoneyEventsAnalysisModule());
+	}
+
+	/**
+	 * Add travel time bindings for ride and freight modes, which are not actually network modes.
+	 */
+	public static final class TravelTimeBinding extends AbstractModule {
+
+		private final boolean carOnly;
+
+		public TravelTimeBinding() {
+			this.carOnly = false;
+		}
+
+		public TravelTimeBinding(boolean carOnly) {
+			this.carOnly = carOnly;
+		}
+
+		@Override
+		public void install() {
+			addTravelTimeBinding(TransportMode.ride).to(networkTravelTime());
+			addTravelDisutilityFactoryBinding(TransportMode.ride).to(carTravelDisutilityFactoryKey());
+
+			if (!carOnly) {
+				addTravelTimeBinding("freight").to(Key.get(TravelTime.class, Names.named(TransportMode.truck)));
+				addTravelDisutilityFactoryBinding("freight").to(Key.get(TravelDisutilityFactory.class, Names.named(TransportMode.truck)));
+
+
+				bind(BicycleLinkSpeedCalculator.class).to(BicycleLinkSpeedCalculatorDefaultImpl.class);
+
+				// Bike should use free speed travel time
+				addTravelTimeBinding(TransportMode.bike).to(BicycleTravelTime.class);
+				addTravelDisutilityFactoryBinding(TransportMode.bike).to(OnlyTimeDependentTravelDisutilityFactory.class);
+			}
+		}
+	}
+
+}
